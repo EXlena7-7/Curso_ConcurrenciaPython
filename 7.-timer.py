88302419cb25c201bddd151598e37f9a2f109d66
EXlena7-7 <marjor071101@gmail.com>
2024-01-15 02:28:17 +0000

    :memo:  tareas programadas con python
     tareas programadas con python

---
+++ 7.-timer.py
@@ -0,0 +1,16 @@
+import logging
+import threading
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
+# Timer
+def callback():
+    logging.info('Hola, soy un callback que no ejecuta de forma inmediata')
+
+
+if __name__ == '__main__':
+    thread = threading.Timer(3, callback)
+    thread.start()
+
+logging.info('Hola, soy el thread principal')
+logging.info('Estamos a la espera de la ejecucion del callback')
